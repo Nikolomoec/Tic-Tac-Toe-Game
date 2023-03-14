8a6b960a4112deea872a3dd92ba1a3909b1f07af
Nikolomoec <nikolomoec@gmail.com>
2023-03-14 15:45:47 +0200

    Initial Commit

---
+++ TicTacToeGame/TicTacToeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeGameApp.swift
+//  TicTacToeGame
+//
+//  Created by Nikita Kolomoec on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
